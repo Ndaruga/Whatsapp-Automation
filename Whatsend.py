fff4f2ba43d9748e3cacbe7be9ac9b9efcd78d0a
ndaruga <ndaruga.francis@gmail.com>
2023-02-06 17:30:06 +0300

    final

---
+++ Whatsend.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  6 17:20:25 2023
+
+@author: User
+"""
+
+import pandas as pd
+import re
+Clients=pd.read_csv('Numbers/Clients.csv')
+
+Clients=Clients.values.tolist()
+type(Clients)
+
+
+client_No=[]
+for i in Clients:
+    string = re.sub('[\W\_]','',str(i))
+    no = '+'+str(string)
+    client_No.append(no)
+    
+    
+del Clients # Delete the dataframe to save memory
+
+
+import time
+import webbrowser as web
+from datetime import datetime
+from re import fullmatch
+
+import pyautogui as pg
+from pywhatkit.core import core, exceptions, log
+
+pg.FAILSAFE = False
+
+core.check_connection()
+
+def open_web() -> bool:
+    """Opens WhatsApp Web"""
+
+    try:
+        web.open("https://web.whatsapp.com")
+    except web.Error:
+        return False
+    else:
+        return True
+
+
+
+def sendwhatmsg(
+        phone_no: str,
+        message: str,
+        time_hour: int,
+        time_min: int,
+        wait_time: int = 20,
+        tab_close: bool = True,
+        close_time: int = 15,
+) -> None:
+    """Send a WhatsApp Message at a Certain Time"""
+    if not core.check_number(number=phone_no):
+        raise exceptions.CountryCodeException("Country Code Missing in Phone Number!")
+
+    phone_no = phone_no.replace(" ", "")
+    if not fullmatch(r'^\+?[0-9]{2,4}\s?[0-9]{9,15}', phone_no):
+        raise exceptions.InvalidPhoneNumber("Invalid Phone Number.")
+
+    if time_hour not in range(25) or time_min not in range(60):
+        raise Warning("Invalid Time Format!")
+
+    current_time = time.localtime()
+    left_time = datetime.strptime(
+        f"{time_hour}:{time_min}:0", "%H:%M:%S"
+    ) - datetime.strptime(
+        f"{current_time.tm_hour}:{current_time.tm_min}:{current_time.tm_sec}",
+        "%H:%M:%S",
+    )
+
+    if left_time.seconds < wait_time:
+        raise exceptions.CallTimeException(
+            "Call Time must be Greater than Wait Time as WhatsApp Web takes some Time to Load!"
+        )
+
+    sleep_time = left_time.seconds - wait_time
+    print(
+        f"In {sleep_time} Seconds WhatsApp will open and Message will be Delivered to {phone_no}!"
+    )
+    
+    time.sleep(sleep_time)
+    core.send_message(message=message, receiver=phone_no, wait_time=wait_time)
+ 
+    if tab_close:
+        core.close_tab(wait_time=2)
+        
+    core.send_image(path=img_path, caption=caption, receiver=phone_no, wait_time=wait_time)
+    
+    log.log_message(_time=current_time, receiver=phone_no, message=message)
+    if tab_close:
+        core.close_tab(wait_time=3)
+
+
+
+def check_if_string_in_file(file_name, string_to_search):
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            if str(string_to_search) in line:
+                return True
+    return False
+
+No = ["+254113927737", "+254784116116", "+254799844628"]
+
+
+message = 'Hello, My name is Francis. I\'m a software developer and an academic tutor. \nIf you feel you need help with your projects,  assignments, quizzes or classes, please don\'t hesitate to reach out.\nFeel free to check my profile and recent projects https://github.com/Ndaruga \nThank you!'
+
+caption="Here is a sample of a student's class performance for your reference"
+img_path= "./Image1.jpeg"
+
+
+
+for i in No:
+    if check_if_string_in_file('PyWhatKit_DB.txt', i) == True:
+#         print(f'{i} already Exists in Database')
+        pass
+    elif check_if_string_in_file('Numbers/Employers.csv', i) == True:
+#         print(f'{i} is an employer')
+        pass
+    else:            
+        tim=datetime.now()
+        try:
+            sendwhatmsg(i, message=message, time_hour=tim.hour, time_min=tim.minute+1)
+        except:
+            print(f'Failed to send message to {i}')
+
+
