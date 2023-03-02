63b5f47b12486db60692040d7571000bd906a5aa
ndaruga <ndaruga.francis@gmail.com>
2023-03-02 17:08:07 +0300

    ---

--- msgsender.py
+++ msgsender.py
@@ -10,7 +10,7 @@ import re
 import os
 import whatsend._core as _core
 import whatsend.core as core
-# import whatsend.cleaner as cleaner
+import whatsend.cleaner as cleaner
 
 Clients=pd.read_table(os.path.join("Numbers", "clients.txt"), header=0)
 
@@ -100,8 +100,6 @@ def check_if_string_in_file(file_name, string_to_search):
                 return True
     return False
 
-
-# No = ["+254113927737", "+254784116116",  "+254113927737",  "+254799844628"]
 
 
 for i in PhoneNumbers:
--- whatsend/cleaner.py
+++ whatsend/cleaner.py
@@ -19,10 +19,9 @@ for i in pd.Series.iteritems(df):
     i=str(i[1])
 #     i=re.sub(' ','',i)
     i=re.sub('[A-Z a-z(-)-+=":_"<>/]','',i)
-#     i=int(i)
+    i=i.split(".")[0]
     if len(i) < 9:
         pass
-    
     elif len(i) > 14:
         i= i[0:13]
     elif ken.fullmatch(i, 0,4) != None:
@@ -41,20 +40,34 @@ if not os.path.exists(os.path.join("Numbers", "Clients.txt")):
 
 # Check numbers repeating more than 2 times add them to an array
 from collections import Counter
-
 counts = dict(Counter(data))
 
-for key, value in counts.items():
-    if value > 2:
-        with open(os.path.join("Numbers", "Employers.txt"), 'a+', encoding="utf-8") as f:
-            if str(key) in f.read():
+emp_file= os.path.join(".", "Numbers", "Employers.txt")
+client_file = os.path.join(".", "Numbers", "Clients.txt")
+
+def check_if_string_in_file(file_name, string_to_search):
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            if str(string_to_search) in line:
+                return True
+    return False
+
+
+sort = sorted(counts.items(), key=lambda x:x[1], reverse=True)
+
+for i in sort:
+    if check_if_string_in_file(emp_file, i[0]) == True:
+        pass
+    elif i[1] > 3:
+        with open(emp_file, 'a+', encoding="utf-8") as f:
+            f.write(f"{i[0]}\n")
+        f.close()
+    else:
+        with open(client_file, 'a+', encoding="utf-8") as f:
+            if check_if_string_in_file(client_file, i[0]) == True:
                 pass
             else:
-                f.write(f"{key}\n")
-                f.close()
-    else:
-        with open(os.path.join("Numbers", "Clients.txt"), 'a', encoding="utf-8") as f:
-            f.write(f"{key}\n")
+                f.write(f"{i[0]}\n")
             f.close()
 
 
